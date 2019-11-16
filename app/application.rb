709c424c773350623ad1743d32c2ff7a431891cf
Joseh <joseph161216@gmail.com>
2019-11-15 16:39:00 -0800

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,26 @@
+require "pry"
+require "time"
+require "date"
 class Application
   
   def call(env)
     resp = Rack::Response.new
-    time = Time.new
-    puts time
+    # time = Time.new(2015, 11, 27, 14, 30)
+    # new_time = DateTime.strptime(time, '%H')
+    # new_time = time.hour
+    # binding.pry
+    # 0
+    # puts new_time
+    # self.each do |time|
+    #   binding.pry
+    #   0
+      if Time.now.hour.to_i <= 12
+        resp.write "Good Morning!"
+      else
+        resp.write "Good Afternoon!"
+      end
+    # end
+    resp.finish
   end
 
 end
